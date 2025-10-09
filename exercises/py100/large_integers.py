5b15c7459e4715b54424968f004b0de1ed53897d
shinhyung-lee <shinlee.dev@gmail.com>
2025-10-08 23:29:56 -0400

    large int print

---
+++ exercises/py100/large_integers.py
@@ -0,0 +1,8 @@
+
+large_number = 9312178542105
+
+# Using comma as a thousands separator
+print(f"Comma separator: {large_number:,}")
+
+# Using underscore as a thousands separator
+print(f"Underscore separator: {large_number:_}")
