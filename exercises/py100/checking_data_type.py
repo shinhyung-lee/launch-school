74e821adcb3cd3ee248207b954027d62e346ffc4
shinhyung-lee <shinlee.dev@gmail.com>
2025-10-08 23:45:16 -0400

    finding out types

---
+++ exercises/py100/checking_data_type.py
@@ -0,0 +1,9 @@
+# checking data types for the following values 
+# use type() function
+
+
+print(type(23.5)) # should print out float
+print(type('Call me Ishmael.')) # should print out str
+print(type(False)) # bool
+print(type(0)) # int
+print(type(None)) # Nonetype
