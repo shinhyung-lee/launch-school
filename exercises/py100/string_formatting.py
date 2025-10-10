0c266c3298631a64a3eb7540d6134589df43a75b
shinhyung-lee <shinlee.dev@gmail.com>
2025-10-09 22:45:15 -0400

    string formatting

---
+++ exercises/py100/string_formatting.py
@@ -0,0 +1,8 @@
+name = 'Victor'
+profession = 'programmer'
+
+# using str.format
+print('Hello, {}. you are a {}.'.format(name, profession))
+
+# using f-string 
+print(f'Hello, {name}. you are a {profession}.')
