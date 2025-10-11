a77e0f75084470d646c4d2df6ffea01870bf52ef
shinhyung-lee <shinlee.dev@gmail.com>
2025-10-11 14:41:54 -0400

    syntax error

---
+++ exercises/py100/syntax_error.py
@@ -0,0 +1,11 @@
+
+# in the original code, we did not have : (colon)
+# we have to use colon when we are done writing the condition in the conditionals
+
+speed_limit = 60
+current_speed = 80
+
+if current_speed > speed_limit:
+    print('"People are so bad at driving cars that '
+          "computers don\'t have to be that good to be "
+          'much better." -- Marc Andreessen')
