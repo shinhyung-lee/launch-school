ebe91143b49e9f40b72ea3ea93acba8b77aa682b
shinhyung-lee <shinlee.dev@gmail.com>
2025-10-11 14:37:18 -0400

    find substring

---
+++ exercises/py100/find_substring.py
@@ -0,0 +1,9 @@
+
+
+# to check whether a string contains a substring in python,
+# use in operator
+
+if "admin" in "mes admin":
+    print('substring test passed')
+else:
+    print('substring not included')
