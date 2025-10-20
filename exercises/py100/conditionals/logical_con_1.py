41d9b0608bf4740f72f227309c22905d797e888e
Shin hyung Lee <shinhyunglee@Mac.lan>
2025-10-20 01:07:22 -0400

    logical condition 1

---
+++ exercises/py100/conditionals/logical_con_1.py
@@ -0,0 +1,11 @@
+'''
+ the below code will print 'Yes!'
+ logical operator or returns true if one of the condition is true in A or B
+ in if False or True, we have True in the latter part of or.
+ this will evaluate to True. That's why it will print out 'Yes!'
+'''
+
+if False or True:
+    print('Yes!')
+else:
+    print('No...')
