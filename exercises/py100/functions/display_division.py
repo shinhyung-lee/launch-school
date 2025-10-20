8b8dd1c97dfea304d1b3f47e4b99dbd4e7a936b5
Shin hyung Lee <shinhyunglee@Mac.lan>
2025-10-20 01:33:49 -0400

    display division

---
+++ exercises/py100/functions/display_division.py
@@ -0,0 +1,11 @@
+# Will not print anything. When we call functions, we have to add parenthese
+# after the function name.
+
+def multiples_of_three():
+    divisor = 1
+
+    for dividend in range(3, 31, 3):
+        print(f'{dividend} / {divisor} = 3')
+        divisor += 1
+
+multiples_of_three
