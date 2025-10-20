51085bd7af7d4e932a2df742330ae973d7d5ecc7
Shin hyung Lee <shinhyunglee@Mac.lan>
2025-10-20 01:11:04 -0400

    logical conditional 2

---
+++ exercises/py100/conditionals/logical_con_2.py
@@ -0,0 +1,9 @@
+# the below code will print out 'No...'
+# logical and evaluates to True only if both operands are True
+# True and False evalutes to False, so the if-else conditional will print 
+# out 'No...'
+
+if True and False:
+    print('Yes!')
+else:
+    print('No...')
