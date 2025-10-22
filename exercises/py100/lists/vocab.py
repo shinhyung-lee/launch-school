9d7b3239b18d3eef365797c775ac5c52ff16dec4
Shin hyung Lee <shinhyunglee@Mac.lan>
2025-10-22 03:25:24 -0400

    vocab

---
+++ exercises/py100/lists/vocab.py
@@ -0,0 +1,20 @@
+
+
+
+vocabulary = [
+    ['happy', 'cheerful', 'merry', 'glad'],
+    ['tired', 'sleepy', 'fatigued', 'drained'],
+    ['excited', 'eager', 'enthused', 'animated'],
+]
+
+for category in vocabulary:
+    for word in category:
+        print(word)
+
+# happy
+# cheerful
+# merry
+# glad
+# tired
+# sleepy
+# etc...
