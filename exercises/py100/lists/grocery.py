b8db36c216a87db421db5de911b9cf2d4e85b832
shinhyung-lee <shinlee.dev@gmail.com>
2025-10-22 17:15:41 -0400

    groceries

---
+++ exercises/py100/lists/grocery.py
@@ -0,0 +1,10 @@
+
+
+grocery_list = ['paprika', 'tofu', 'garlic', 'quinoa',
+                'carrots', 'broccoli', 'hummus']
+
+while len(grocery_list) > 0:
+    removed = grocery_list.pop(0)
+    print(removed)
+
+print(grocery_list)
