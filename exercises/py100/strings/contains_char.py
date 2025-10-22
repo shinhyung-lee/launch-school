f2a2b3cde8ea40b0c3119b0fded3d8a18b89ef13
Shin hyung Lee <shinhyunglee@Mac.lan>
2025-10-22 02:56:50 -0400

    contains char

---
+++ exercises/py100/strings/contains_char.py
@@ -0,0 +1,9 @@
+
+
+char_sequence = 'TXkgaG92ZXJjcmFmdCBpcyBmdWxsIG9mIGVlbHMu'
+char_to_find = 'x'
+
+if char_sequence.find(char_to_find):
+    print('string contains x')
+else:
+    print('string does not contain x')
