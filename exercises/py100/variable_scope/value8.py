a4c10b0c98aca805f87258078dec95395289255a
shinhyung-lee <shinlee.dev@gmail.com>
2025-10-21 21:56:20 -0400

    value8

---
+++ exercises/py100/variable_scope/value8.py
@@ -0,0 +1,6 @@
+# program will not print out anything
+# variable initialization came after the print statement, so the program does not know what greeting variable is
+
+print(greeting)
+
+greeting = 'Hello world!'
