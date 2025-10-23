2f08501ab7927d3661ac985016cb0f8d24c8c5af
shinhyung-lee <shinlee.dev@gmail.com>
2025-10-22 21:42:49 -0400

    confucius says

---
+++ exercises/py100/debugging/c_says.py
@@ -0,0 +1,14 @@
+
+# the problem with the original function is that it's not returning anything
+# simply add return to each if statement in get_quote function
+
+def get_quote(person):
+    if person == 'Yoda':
+        return 'Do. Or do not. There is no try.'
+    if person == 'Confucius':
+        return 'I hear and I forget. I see and I remember. I do and I understand.'
+    if person == 'Einstein':
+        return 'Do not worry about your difficulties in Mathematics. I can assure you mine are still greater.'
+
+print('Confucius says:')
+print('"' + get_quote('Confucius') + '"')
