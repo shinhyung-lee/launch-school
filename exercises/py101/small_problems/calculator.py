2b283ae10b6a7fdfcc9fba7b00bb2573844c8b64
Shin hyung Lee <shinhyunglee@Mac.lan>
2025-10-28 11:40:22 -0400

    calculator refactor

--- exercises/py101/small_problems/calculator.py
+++ exercises/py101/small_problems/calculator.py
@@ -1,30 +1,35 @@
 
 
+def prompt(message):
+    print(f"==> {message}")
+    
 # Ask the user for the second number 
 # Ask the user for an operator to perform
 # Perform the operation on the two numbers
 # Print the result to the terminal
 
-print('Welcome to Calculator!')
+prompt('Welcome to Calculator!')
 
 # Ask the user for the first number.
 
-number1 = int(input("What's the first number? "))
+prompt("What's the first number? ")
+number1 = int(input())
 
-number2 = int(input("What's the second number? "))
+prompt("What's the second number? ")
+number2 = int(input())
 
-print(f'{number1} {number2}')
+operation = input('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide: ')
 
-operation = input('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide ')
-
-if operation == '1':
-    output = number1 + number2 
-elif operation == '2':
-    output = number1 - number2 
-elif operation == '3':
-    output = number1 * number2 
-elif operation == '4':
-    output = number1 / number2 
+match operation:
+    case '1':
+        output = number1 + number2 
+    case '2':
+        output = number1 - number2 
+    case '3':
+        output = number1 * number2 
+    case '4':
+        output = number1 / number2 
+        
     
 print(f"The result is: {output}")
 
