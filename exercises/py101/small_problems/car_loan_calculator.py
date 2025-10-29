6364d2a809fbc878b8d12568fc2180286bb0d967
shinhyung-lee <shinlee.dev@gmail.com>
2025-10-28 21:39:43 -0400

    car loan tweak

--- exercises/py101/small_problems/car_loan_calculator.py
+++ exercises/py101/small_problems/car_loan_calculator.py
@@ -21,25 +21,38 @@ def invalid_loan_or_apr(number) -> bool:
     
     return False # Valid number
 
-# cannot be a negative number, str, or 0 
-loan_amt = input("Enter loan amount: ")
-while invalid_loan(loan_amt):
-    print("Invalid number entered. Try again.")
+while True:
+    # cannot be a negative number, str, or 0 
     loan_amt = input("Enter loan amount: ")
+    while invalid_loan(loan_amt):
+        print("Invalid number entered. Try again.")
+        loan_amt = input("Enter loan amount: ")
 
-# cannot be a negative number, str
-apr_rate = input("Enter your APR rate in percent (ex: 5%, enter 5): ") 
-while invalid_loan_or_apr(apr_rate):
-    print("Invalid number entered. Try again.")
-    apr_rate = input("Enter your APR rate in percent (ex: 5%, enter 5): ")
+    # cannot be a negative number, str
+    apr_rate = input("Enter your APR rate in percent (ex: 5%, enter 5): ") 
+    while invalid_loan_or_apr(apr_rate):
+        print("Invalid number entered. Try again.")
+        apr_rate = input("Enter your APR rate in percent (ex: 5%, enter 5): ")
 
-# cannot be a negative number, str
-loan_duration = input("Enter the loan duration in months: ")
-while invalid_loan_or_apr(loan_duration):
-    print("Invalid number entered. Try again.")
+    # cannot be a negative number, str
     loan_duration = input("Enter the loan duration in months: ")
+    while invalid_loan_or_apr(loan_duration):
+        print("Invalid number entered. Try again.")
+        loan_duration = input("Enter the loan duration in months: ")
 
-monthly_interest_rate = float(apr_rate) / 12 / 100
+    monthly_interest_rate = float(apr_rate) / 12 / 100
 
-monthly_pay = float(loan_amt) * (monthly_interest_rate / (1 - (1 + monthly_interest_rate) ** (-1 * float(loan_duration))))
-print(f"Monthly payment will be {monthly_pay}")
+    monthly_pay = float(loan_amt) * (monthly_interest_rate / (1 - (1 + monthly_interest_rate) ** (-1 * int(loan_duration))))
+    print(f"Monthly payment will be {monthly_pay}")
+
+    print("Another calculation?")
+    answer = input().lower()
+    while True:
+        if answer.startswith('n') or answer.startswith('y'):
+            break 
+        
+        print('Please enter "y" or "n".')
+        answer = input().lower()
+        
+    if answer[0] == 'n':
+        break
