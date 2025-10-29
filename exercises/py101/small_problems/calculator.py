3a5e2cfd3bb8b28a24766739a3af8ddeedee097f
shinhyung-lee <shinlee.dev@gmail.com>
2025-10-28 20:14:56 -0400

    calculator refined with json

--- exercises/py101/small_problems/calculator.py
+++ exercises/py101/small_problems/calculator.py
@@ -1,40 +1,80 @@
+import json
 
+LANGUAGE = 'en'
+
+with open("config.json", 'r') as file:
+    prompts = json.load(file)
 
 def prompt(message):
     print(f"==> {message}")
+
+def invalid_number(number_str):
+    try:
+        float(number_str)
+    except ValueError:
+        return True 
+
+    return False 
+
+def invalid_bool(bool_str):
+    if bool_str.casefold() in ['y', 'n']:
+        return False 
+    else:
+        return True 
+
+program_continue = True 
+
+prompt('Welcome to Calculator!\n Bienvenido, elige tu idioma\n')
+language = input("Choose language. 1) English 2) Espanol: ")
+
+while language not in ["1", "2"]:
+    prompt("Invalid number.")
+    language = input("Choose language. 1) English 2) Espanol: ")
+
+lang = "en" if language == "1" else "es"
+
+while program_continue:
+
+    prompt(prompts[lang]["first number"])
+    number1 = input()
+    while invalid_number(number1):
+        prompt(prompts[lang]["invalid number"])
+        number1 = input()
+
+    prompt(prompts[lang]["second number"])
+    number2 = input()
+
+    while invalid_number(number2):
+        prompt(prompts[lang]["invalid number"])
+        number2 = input()
+
+    operation = input(prompts[lang]["choose operation"])
+
+    match operation:
+        case '1':
+            output = float(number1) + float(number2) 
+        case '2':
+            output = float(number1) - float(number2) 
+        case '3':
+            output = float(number1) * float(number2) 
+        case '4':
+            output = float(number1) / float(number2) 
     
-# Ask the user for the second number 
-# Ask the user for an operator to perform
-# Perform the operation on the two numbers
-# Print the result to the terminal
+    while operation not in ["1", "2", "3", "4"]:
+        print("incorrect operation chosen. Try again.")
+        operation = input(prompts[lang]["choose operation"])
 
-prompt('Welcome to Calculator!')
+    print(f"The result is: {output}")
 
-# Ask the user for the first number.
+    # answer
+    program_continue_raw = input(prompts[lang]["continue message"])
+    # needs to be Y/N or y/n (use casefold)
+    # if not valid bool, prompt user to enter continue value again
+    while invalid_bool(program_continue_raw):
+        print("incorrect boolean value chosen. Try again.")
+        program_continue_raw = input(prompts[lang]["continue message"])
 
-prompt("What's the first number? ")
-number1 = int(input())
-
-prompt("What's the second number? ")
-number2 = int(input())
-
-operation = input('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide: ')
-
-match operation:
-    case '1':
-        output = number1 + number2 
-    case '2':
-        output = number1 - number2 
-    case '3':
-        output = number1 * number2 
-    case '4':
-        output = number1 / number2 
-        
-    
-print(f"The result is: {output}")
-
-
-
+    program_continue = (program_continue_raw.casefold() == 'y')
 
 
 
