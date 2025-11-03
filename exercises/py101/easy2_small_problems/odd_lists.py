90352ef79e23d4f1c677ab84559d729f4f887863
shinhyung-lee <shinlee.dev@gmail.com>
2025-11-03 17:05:47 -0500

    odd lists

---
+++ exercises/py101/easy2_small_problems/odd_lists.py
@@ -0,0 +1,10 @@
+
+
+def oddities(lst):
+    return [elem for (index, elem) in enumerate(lst) if index % 2 == 0]
+
+print(oddities([2, 3, 4, 5, 6]) == [2, 4, 6])  # True
+print(oddities([1, 2, 3, 4]) == [1, 3])        # True
+print(oddities(["abc", "def"]) == ['abc'])     # True
+print(oddities([123]) == [123])                # True
+print(oddities([]) == [])                      # True
