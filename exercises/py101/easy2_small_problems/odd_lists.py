4333558f8f4a60820d10754d7c889c60534ebf92
shinhyung-lee <shinlee.dev@gmail.com>
2025-11-03 17:51:05 -0500

    random age

--- exercises/py101/easy2_small_problems/odd_lists.py
+++ exercises/py101/easy2_small_problems/odd_lists.py
@@ -8,3 +8,12 @@ print(oddities([1, 2, 3, 4]) == [1, 3])        # True
 print(oddities(["abc", "def"]) == ['abc'])     # True
 print(oddities([123]) == [123])                # True
 print(oddities([]) == [])                      # True
+
+def oddities2(lst):
+    return lst[::2]
+
+print(oddities2([2, 3, 4, 5, 6]) == [2, 4, 6])  # True
+print(oddities2([1, 2, 3, 4]) == [1, 3])        # True
+print(oddities2(["abc", "def"]) == ['abc'])     # True
+print(oddities2([123]) == [123])                # True
+print(oddities2([]) == [])                      # True
