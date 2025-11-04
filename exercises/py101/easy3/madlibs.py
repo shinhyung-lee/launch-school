ad57a1fb5d4531741b669f48d23d1dc9bf0a040d
shinhyung-lee <shinlee.dev@gmail.com>
2025-11-04 16:38:13 -0500

    madlibs

---
+++ exercises/py101/easy3/madlibs.py
@@ -0,0 +1,14 @@
+
+
+def get_madlib():
+    noun = input("Enter a noun: ")
+    verb = input("Enter a verb: ")
+    adj = input("Enter an adjective: ")
+    adverb = input("Enter an adverb: ")
+
+    print(f"Do you {verb} your {adj} {noun} {adverb}? That's hilarious!")
+    print(f"The {adj} {noun} {verb}s {adverb} over the lazy {noun}.")
+    print(f"The {noun} {adverb} {verb}s up to Joe's {adj} turtle.")
+    
+
+get_madlib()
