80cf1fd11ea1e744c9b94c5a0e810e3d06d6c472
shinhyung-lee <shinlee.dev@gmail.com>
2025-11-03 20:52:01 -0500

    collapse dulicates

---
+++ exercises/py101/easy3/collapse_duplicates.py
@@ -0,0 +1,19 @@
+
+
+def crunch(sentence):
+    # result string
+    result = ''
+    # iterate using index, if s[curr] != s[curr + 1], add to string
+    for idx, _ in enumerate(sentence):
+        if idx == len(sentence) - 1 or sentence[idx] != sentence[idx + 1]:
+            result += sentence[idx]
+    return result
+        
+
+# These examples should all print True
+print(crunch('ddaaiillyy ddoouubbllee') == 'daily double')
+print(crunch('4444abcabccba') == '4abcabcba')
+print(crunch('ggggggggggggggg') == 'g')
+print(crunch('abc') == 'abc')
+print(crunch('a') == 'a')
+print(crunch('') == '')
