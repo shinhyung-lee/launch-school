79ee4120d0478331492c98254c91211df25036c1
shinhyung-lee <shinlee.dev@gmail.com>
2025-11-03 20:11:53 -0500

    middle char edited

--- exercises/py101/easy2_small_problems/middle_char.py
+++ exercises/py101/easy2_small_problems/middle_char.py
@@ -1,19 +1,10 @@
-def center_of_even_sentence(sentence):
-    # launch len(sentence) // 2 = 3 - 1
-    left_index = len(sentence) // 2 - 1
-    return sentence[left_index:left_index + 2]
-
-def center_of_odd_sentence(sentence):
-    # odd len(sentence) // 2 = 1
-    center_index = len(sentence) // 2 
-    return sentence[center_index]
-
 def center_of(sentence):
-    # assume len(sentence) > 0 
     if len(sentence) % 2 == 1:
-        return center_of_odd_sentence(sentence)
+        center_index = len(sentence) // 2
+        return sentence[center_index]
     else:
-        return center_of_even_sentence(sentence)
+        left_index = len(sentence) // 2 - 1
+        return sentence[left_index:left_index + 2]
 
 print(center_of('I Love Python!!!') == "Py")    # True
 print(center_of('Launch School') == " ")        # True
