40218a81c32bf6bf90012d7495f2281b20555a3d
Shin hyung Lee <shinhyunglee@Mac.lan>
2025-11-26 04:15:02 -0500

    ttt finished

--- exercises/py110/lesson3/tictactoe.py
+++ exercises/py110/lesson3/tictactoe.py
@@ -4,6 +4,7 @@ import os
 def display_board(board):
     os.system('clear')
     
+    prompt(f"You are {HUMAN_MARKER}. Computer is {COMPUTER_MARKER}.")
     print('')
     print('     |     |')
     print(f"  {board[1]}  |  {board[2]}  |  {board[3]}")
@@ -53,17 +54,54 @@ def computer_choose_square(board):
 def board_full(board):
     return len(empty_squares(board)) == 0
 
+def detect_winner(board):
+    winning_lines = [
+        [1, 2, 3], [4, 5, 6], [7, 8, 9],  # rows
+        [1, 4, 7], [2, 5, 8], [3, 6, 9],  # columns
+        [1, 5, 9], [3, 5, 7]              # diagonals
+    ]
+    for line in winning_lines:
+        sq1, sq2, sq3 = line
+        if (board[sq1] == HUMAN_MARKER
+               and board[sq2] == HUMAN_MARKER
+               and board[sq3] == HUMAN_MARKER):
+            return 'Player'
+        elif (board[sq1] == COMPUTER_MARKER
+                  and board[sq2] == COMPUTER_MARKER
+                  and board[sq3] == COMPUTER_MARKER):
+            return 'Computer'
+
+    return None
+
 def someone_won(board):
-    return False 
+    return bool(detect_winner(board))
 
-board = initialize_board()
-display_board(board)
 
-while True:
-    player_choose_square(board)
-    computer_choose_square(board)
-    display_board(board)
+def play_tic_tac_toe():
+    while True:
+        board = initialize_board()
+        while True:
+            display_board(board)
+            
+            player_choose_square(board)
+            if someone_won(board) or board_full(board):
+                break 
+            
+            computer_choose_square(board)
+            if someone_won(board) or board_full(board):
+                break 
+        
+        display_board(board)
+        if someone_won(board):
+            prompt(f"{detect_winner(board)} won!")
+        else:
+            prompt("It's a tie!")
+        prompt("Play again? (y or n)")
+        answer = input().lower() 
+        
+        if answer[0] != 'y':
+            break 
     
-    if someone_won(board) or board_full(board):
-        break 
-    
+    prompt('Thanks for playing Tic Tac Toe!')
+
+play_tic_tac_toe()
