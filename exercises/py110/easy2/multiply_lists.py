556eecfcb3865b24ab86b18eb9a915302d9cee5e
shinhyung-lee <shinlee.dev@gmail.com>
2025-12-03 17:57:03 -0500

    multiply lists

---
+++ exercises/py110/easy2/multiply_lists.py
@@ -0,0 +1,37 @@
+'''
+PEDAC
+Input:  takes two list arguments, each containing a list of numbers
+Output: a new list that contains the product of each pair of numbers from 
+        the arguments that have the same index.
+
+Rules:
+Explicit:
+    -arguments contain the same number of elements 
+
+Questions:
+    - can lists be empty? 
+
+Data Structures:
+
+Algorithms:
+    - initialize an empty list "multiplied"
+    - iterate through both lists using the length of one list
+        - declare a variable "multiple" as a multiplication of numbers from list1 and list2 on the same index
+        - append "multiple" to "multiplied"
+    - return "multiplied"
+'''
+
+def multiply_list(lst1, lst2):
+    multiplied = []
+    for idx in range(len(lst1)):
+        multiple = lst1[idx] * lst2[idx]
+        multiplied.append(multiple)
+    return multiplied
+
+list1 = [3, 5, 7]
+list2 = [9, 10, 11]
+print(multiply_list(list1, list2) == [27, 50, 77])  # True
+
+list1 = []
+list2 = []
+print(multiply_list(list1, list2) == [])  # True
