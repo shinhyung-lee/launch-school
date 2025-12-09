4de01f986ba5616989e4b8ebe7531e6e63212919
Shin hyung Lee <shinhyunglee@Shins-MacBook-Air.local>
2025-12-09 15:59:51 -0500

    rotation pt1

---
+++ exercises/py110/medium1/rotation_pt1.py
@@ -0,0 +1,46 @@
+'''
+Input:  A list, or possibly other data type object 
+Output: A list, move the first element to the end of the list. 
+
+Rules:
+Explicit:
+- If the input is an empty list, return an empty list
+- If the input is not a list, return None 
+- Do not modify the original list. Return a new_list instead
+
+Data Structures:
+- List:
+- type checking: type()
+
+Algorithms:
+- if type(lst) is not list, return None
+- if lst == [], return [] 
+- if len(lst) == 1, return lst 
+- new_list = lst[1:] + lst[0]
+- return new new_list
+'''
+def rotate_list(lst):
+    if type(lst) is not list:
+        return None 
+    if lst == []:
+        return []
+    # if len(lst) == 1:
+    #     return lst 
+    return lst[1:] + [lst[0]]
+
+# All of these examples should print True
+print(rotate_list([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7])
+print(rotate_list(['a', 'b', 'c']) == ['b', 'c', 'a'])
+print(rotate_list(['a']) == ['a'])
+print(rotate_list([1, 'a', 3, 'c']) == ['a', 3, 'c', 1])
+print(rotate_list([{'a': 2}, [1, 2], 3]) == [[1, 2], 3, {'a': 2}])
+print(rotate_list([]) == [])
+
+# return `None` if the argument is not a list
+print(rotate_list(None) == None)
+print(rotate_list(1) == None)
+
+# the input list is not mutated
+lst = [1, 2, 3, 4]
+print(rotate_list(lst) == [2, 3, 4, 1])
+print(lst == [1, 2, 3, 4])
