6e9ee95697e752c2d6353fda7758ee88b852e725
Shin hyung Lee <shinhyunglee@Shins-MacBook-Air.local>
2025-12-12 17:34:39 -0500

    transpose 3 * 3 edited

--- exercises/py110/advanced/transpose_3_3.py
+++ exercises/py110/advanced/transpose_3_3.py
@@ -26,11 +26,11 @@ Pseudocode
 '''
 
 def transpose(matrix):
-    new_list = [['', '', ''], ['', '', ''], ['', '', '']]
+    transposed = [['', '', ''], ['', '', ''], ['', '', '']]
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
-            new_list[j][i] = matrix[i][j]
-    return new_list 
+            transposed[j][i] = matrix[i][j]
+    return transposed
 
 matrix = [
     [1, 5, 8],
@@ -39,6 +39,30 @@ matrix = [
 ]
 
 new_matrix = transpose(matrix)
+
+print(new_matrix == [[1, 4, 3], [5, 7, 9], [8, 2, 6]]) # True
+print(matrix == [[1, 5, 8], [4, 7, 2], [3, 9, 6]])     # True
+
+def transpose2(matrix):
+    transposed = [] 
+    new_rows_count = len(matrix[0]) 
+    
+    for _ in range(new_rows_count):
+        transposed.append([]) # [[], [], []]
+    
+    for row_idx in range(len(matrix)):
+        for col_idx in range(len(matrix[row_idx])):
+            transposed[col_idx].append(matrix[row_idx][col_idx])
+    
+    return transposed
+
+matrix = [
+    [1, 5, 8],
+    [4, 7, 2],
+    [3, 9, 6],
+]
+
+new_matrix = transpose2(matrix)
 
 print(new_matrix == [[1, 4, 3], [5, 7, 9], [8, 2, 6]]) # True
 print(matrix == [[1, 5, 8], [4, 7, 2], [3, 9, 6]])     # True
