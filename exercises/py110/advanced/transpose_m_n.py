999968df9d59bc6df12d2bbe7e1308d9e3014218
Shin hyung Lee <shinhyunglee@Shins-MacBook-Air.local>
2025-12-12 17:47:25 -0500

    transpose_m_n_matrix

---
+++ exercises/py110/advanced/transpose_m_n.py
@@ -0,0 +1,48 @@
+'''
+Input:  M * N matrix
+Output: Transposed M * N matrix
+
+Rules
+Explicit
+- input matrix has at least one row and column 
+Implicit
+
+Questions
+
+Data Structures
+
+Algorithms
+'''
+
+def transpose(matrix):
+    transposed = [] 
+    new_rows_count = len(matrix[0]) # still correct
+    
+    for _ in range(new_rows_count):
+        transposed.append([]) # [[], [], []]
+    
+    for row_idx in range(len(matrix)):
+        for col_idx in range(len(matrix[row_idx])):
+            transposed[col_idx].append(matrix[row_idx][col_idx])
+    
+    return transposed
+
+# All of these examples should print True
+print(transpose([[1, 2, 3, 4]]) == [[1], [2], [3], [4]])
+print(transpose([[1], [2], [3], [4]]) == [[1, 2, 3, 4]])
+print(transpose([[1]]) == [[1]])
+
+matrix_3_by_5 = [
+    [1, 2, 3, 4, 5],
+    [4, 3, 2, 1, 0],
+    [3, 7, 8, 6, 2],
+]
+expected_result = [
+    [1, 4, 3],
+    [2, 3, 7],
+    [3, 2, 8],
+    [4, 1, 6],
+    [5, 0, 2],
+]
+
+print(transpose(matrix_3_by_5) == expected_result)
