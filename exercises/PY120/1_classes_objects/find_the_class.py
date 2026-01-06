e0fe80a8c588adb633dd796b7fb051b71e926334
Shin hyung Lee <shinhyunglee@Shins-MacBook-Air.local>
2026-01-05 16:02:37 -0800

    find the class

---
+++ exercises/PY120/1_classes_objects/find_the_class.py
@@ -0,0 +1,6 @@
+
+
+# Comments show expected output
+print(type("Hello"))                # <class 'str'>
+print(type(5))                      # <class 'int'>
+print(type([1, 2, 3]))              # <class 'list'>
