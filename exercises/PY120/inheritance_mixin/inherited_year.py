17e47818d75edeaa8f9f56e63a0de913420d9f37
Shin hyung Lee <shinhyunglee@Shins-MacBook-Air.local>
2026-01-06 18:00:44 -0800

    inherited year

---
+++ exercises/PY120/inheritance_mixin/inherited_year.py
@@ -0,0 +1,22 @@
+class Vehicle:
+    def __init__(self, year):
+        self._year = year
+
+    @property
+    def year(self):
+        return self._year
+
+class Truck(Vehicle):
+    def __init__(self, year):
+        super().__init__(year)
+        
+class Car(Vehicle):
+    def __init__(self, year):
+        super().__init__(year)
+
+# Comments show expected output
+truck1 = Truck(1994)
+print(truck1.year)            # 1994
+
+car1 = Car(2006)
+print(car1.year)              # 2006
