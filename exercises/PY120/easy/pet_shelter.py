9d522647dec8ae224ce225f8666f5e6ca7111e28
Shin hyung Lee <shinhyunglee@Shins-MacBook-Air.local>
2026-01-07 16:59:42 -0800

    refactor vehicles

--- exercises/PY120/easy/pet_shelter.py
+++ exercises/PY120/easy/pet_shelter.py
@@ -1,66 +1,39 @@
+class Pet:
+    def __init__(self, animal, name):
+        self.animal = animal
+        self.name = name 
+        
+    def info(self):
+        return f"a {self.animal} named {self.name}"
+    
 class Owner:
     def __init__(self, name):
         self.name = name 
-        self.num_pets = 0
+        self.pets = []
         
-    @property 
-    def name(self):
-        return self._name 
-    
-    @name.setter 
-    def name(self, name):
-        self._name = name 
+    def add_pet(self, pet):
+        self.pets.append(pet)
         
     def number_of_pets(self):
-        return self.num_pets 
-        
-class Pet:
-    def __init__(self, species, name):
-        self.species = species 
-        self.name = name 
-        
+        return len(self.pets)
+    
+    def print_pets(self):
+        for pet in self.pets:
+            print(pet.info())
+            
+    
 class Shelter:
     def __init__(self):
-        self.adopted_pets = {}
-
-    def adopt(self, owner, pet):
-        owner.num_pets += 1 
-        if owner not in self.adopted_pets:
-            self.adopted_pets[owner] = [pet]
-        else:
-            self.adopted_pets[owner].append(pet)
-    
-    def print_adoptions(self):
-        for owner in self.adopted_pets:
-            print(f'{owner.name} has adopted the following pets:')
-            pets = self.adopted_pets[owner]
-            for pet in pets:
-                print(f'a {pet.species} named {pet.name}')
-            print()
+        self.owners = {}
         
-
-cocoa   = Pet('cat', 'Cocoa')
-cheddar = Pet('cat', 'Cheddar')
-darwin  = Pet('bearded dragon', 'Darwin')
-kennedy = Pet('dog', 'Kennedy')
-sweetie = Pet('parakeet', 'Sweetie Pie')
-molly   = Pet('dog', 'Molly')
-chester = Pet('fish', 'Chester')
-
-phanson = Owner('P Hanson')
-bholmes = Owner('B Holmes')
-
-shelter = Shelter()
-shelter.adopt(phanson, cocoa)
-shelter.adopt(phanson, cheddar)
-shelter.adopt(phanson, darwin)
-shelter.adopt(bholmes, kennedy)
-shelter.adopt(bholmes, sweetie)
-shelter.adopt(bholmes, molly)
-shelter.adopt(bholmes, chester)
-
-shelter.print_adoptions()
-print(f"{phanson.name} has {phanson.number_of_pets()} "
-      "adopted pets.")
-print(f"{bholmes.name} has {bholmes.number_of_pets()} "
-      "adopted pets.")
+    def adopt(self, owner, pet):
+        owner.add_pet(pet)
+        if owner.name not in self.owners:
+            self.owners[owner.name] = owner 
+            
+    def print_adoptions(self):
+        for name, owner in self.owners.items():
+            print(f'{name} has adopted the following pets:')
+            owner.print_pets()
+            print()
+    
---
+++ exercises/PY120/easy/refactor_vehicles.py
@@ -0,0 +1,33 @@
+class Vehicle:
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
+    
+    def info(self):
+        return f"{self.make} {self.model}"
+        
+class Car(Vehicle):
+    def __init__(self, make, model):
+        super().__init__(make, model)
+
+    def get_wheels(self):
+        return 4
+
+class Motorcycle(Vehicle):
+    def __init__(self, make, model):
+        super().__init__(make, model)
+        
+    def get_wheels(self):
+        return 2
+
+class Truck(Vehicle):
+    def __init__(self, make, model, payload, wheels = 6):
+        super().__init__(make, model)
+        self.payload = payload
+
+    def get_wheels(self):
+        return 6
+
+t = Truck('Volvo', 'T280', '5T')
+print(t.get_wheels())
+print(t.info())
