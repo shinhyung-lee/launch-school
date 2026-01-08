6fab86aebb9fa33ed2a2a790c42110377fd04936
Shin hyung Lee <shinhyunglee@Shins-MacBook-Air.local>
2026-01-07 17:05:50 -0800

    moving

---
+++ exercises/PY120/easy/moving.py
@@ -0,0 +1,33 @@
+class WalkMixin:
+    def walk(self):
+        return f'{self.name} {self.gait()} forward' 
+
+class Person(WalkMixin):
+    def __init__(self, name):
+        self.name = name
+
+    def gait(self):
+        return "strolls"
+
+class Cat(WalkMixin):
+    def __init__(self, name):
+        self.name = name
+
+    def gait(self):
+        return "saunters"
+
+class Cheetah(WalkMixin):
+    def __init__(self, name):
+        self.name = name
+
+    def gait(self):
+        return "runs"
+    
+mike = Person("Mike")
+print(mike.walk())  # Expected: "Mike strolls forward"
+
+kitty = Cat("Kitty")
+print(kitty.walk())  # Expected: "Kitty saunters forward"
+
+flash = Cheetah("Flash")
+print(flash.walk())  # Expected: "Flash runs forward"
