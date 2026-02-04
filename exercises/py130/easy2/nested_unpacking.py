7f7c022a16549f781393a96873838870fdb90b52
shinhyung-lee <shinlee.dev@gmail.com>
2026-02-03 16:04:40 -0800

    nested unpacking

---
+++ exercises/py130/easy2/nested_unpacking.py
@@ -0,0 +1,13 @@
+'''
+Given a nested tuple data = ((1, 2), (3, 4), (5, 6)), write some code to unpack this tuple into individual variables a, b, c, d, e, f.
+'''
+
+data = ((1, 2), (3, 4), (5, 6))
+
+(a, b), (c, d), (e, f) = data 
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
