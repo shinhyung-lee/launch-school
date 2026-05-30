cfbb8d96f05fc2d2b981b33c83e65c9f76d77f36
shinhyung-lee <shinlee.dev@gmail.com>
2026-05-29 20:37:09 -0700

    utf-16 problem

---
+++ exercises/js210/easy1/multiples_3_5.js
@@ -0,0 +1,16 @@
+
+function multisum(num) {
+  let sum = 0;
+  for (let i = 1; i <= num; i += 1) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+    }
+  }
+  console.log(sum);
+  return sum;
+}
+
+multisum(3);       // 3
+multisum(5);       // 8
+multisum(10);      // 33
+multisum(1000);    // 234168
---
+++ exercises/js210/easy1/utf_16_string.js
@@ -0,0 +1,23 @@
+
+
+function utf16Value(string) {
+  let total = 0;
+  for (let char of string) {
+    let utfVal = char.charCodeAt(0);
+    total += utfVal;
+  }
+  console.log(total);
+  return total;
+}
+
+utf16Value('Four score');         // 984
+utf16Value('Launch School');      // 1251
+utf16Value('a');                  // 97
+utf16Value('');                   // 0
+
+// The next three lines demonstrate that the code
+// works with non-ASCII characters from the UTF-16
+// character set.
+const OMEGA = "\u03A9";             // UTF-16 character 'Ω' (omega)
+utf16Value(OMEGA);                  // 937
+utf16Value(OMEGA + OMEGA + OMEGA);  // 2811
