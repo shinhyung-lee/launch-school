d3b4e7ef1dbb65c7239c2e2ec1734e05ac6873ec
XiaoPigu <xiaopigu@XiaoPigus-MacBook-Air.local>
2026-09-24 11:05:42 -0700

    update

---
+++ exercises/js225/final-study-guide.md
@@ -0,0 +1,9 @@
+
+
+## Arrow functions and context loss
+- Objects do not create a scope of its own.
+- Functions do.
+- Arrow functions capture the context of enclosing function at the time of its creation.
+
+## `class` syntax
+- class is not hoisted
--- exercises/js225/test.js
+++ exercises/js225/test.js
@@ -1,8 +1,38 @@
 
-function reassignArr(arr) {
-  arr.push(2);
+function extend(destination) {
+  let objects = [...arguments].slice(1);
+  for (let obj of objects) {
+    for (let prop of Object.getOwnPropertyNames(obj)) {
+      destination[prop] = obj[prop];
+    }
+  }
+
+  return destination;
 }
 
-let a = [1];
-reassignArr(a);
-console.log(a); // [ 1, 2 ]
+let foo = {
+  a: 0,
+  b: {
+    x: 1,
+    y: 2,
+  },
+};
+
+let joe = {
+  name: 'Joe'
+};
+
+let funcs = {
+  sayHello() {
+    console.log('Hello, ' + this.name);
+  },
+
+  sayGoodBye() {
+    console.log('Goodbye, ' + this.name);
+  },
+};
+
+let object = extend({}, foo, joe, funcs);
+
+console.log(object.b.x);          // => 1
+object.sayHello();                // => Hello, Joe
